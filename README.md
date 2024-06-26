3350b4d8b173b996ec5f915d2e3b5a77c4953770
Elysian-C <nicolas.guerrero.icochea@gmail.com>
2024-06-26 08:57:04 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+<!-- HEADER PROJECT LOGO -->
+<div align="center">
+  <a href="https://github.com/EduardoPuglisevich/Aplicaciones-de-Data-Science.git">
+    <img src="img/UPC_logo.png" alt="Logo UPC" width="80" height="80">
+  </a>
+
+  <h3 align="center"> Aplicaciones-de-Data-Science-CC92</h3>
+
+  <p align="center">
+    Modelo de clasificación y recomendación de tesis de pregrado UPC
+    <br/>
+    <a href=""><strong>Explore the docs »</strong></a>
+    <br/>
+    <br/>
+    <strong>Docente</strong>
+    <br/>
+    Patricia Daniela Reyes Silva
+  </p>
+  <p align="center">
+    Objetivo: Diseñar un modelo de clasificación para clasificar trabajos académicos de la UPC y diseñar un modelo de recomendación para poder obtener textos similares a uno seleccionado.
+  </p>
+  <br/>
+  <a href="https://docs.google.com/document/d/1ZLV04htsHfDt8a1REZJzgE-mr9IwTQdOw6Zph7msCbU/edit?usp=sharing" target="_blank"><strong>Para conocer la investigación del proyecto dar click</strong></a>
+  <br/>
+  <a href="https://dataappweb-production.up.railway.app" target="_blank"><strong>Link app web</strong></a>
+  <br/>
+</div>
+<!-- CONCLUSIONES -->
+
+<h3>Conclusiones</h4>
+<hr/>
+
+- Para concluir con esta investigación, podemos mencionar que se han obtenido los resultados esperados; puesto que, para el sistema de clasificación, logramos obtener métricas superiores al 90% en los casos de prueba, destacando el modelo de Naive Bayes y Regresión Logística. Sin embargo, consideramos que dicha puntuación puede ser mejorada si se logra realizar una mejor limpieza de los datos de entrada, ya que sería mucho mejor si solo entrenará con datos de un solo idioma, además que podríamos implementar otros algoritmos como las redes neuronales para corroborar y comprobar su rendimiento.
+- Por otro lado, para el modelo de recomendación, consideramos que logró su objetivo, ya que logra brindar recomendaciones que presentan temas de interés similares o que pertenecen a la misma facultad del trabajo brindado. No obstante, consideramos que aún podemos afinar más las recomendaciones, ya que existen casos en los que las recomendaciones brindadas se alejan demasiado de la tesis que se le brinda al sistema. Para solucionar esto, pensamos que sería óptimo incrementar la cantidad de datos del dataset, así como buscar nuevos algoritmos que puedan hallar las similitudes entre dos textos de mejor manera.
+- Finalmente, para investigaciones futuras, nos gustaría poder implementar estos sistemas de clasificación y recomendación en datasets mucho más grandes y poder categorizar los trabajos de investigación en etiquetas de interés, algo similar a extraer las palabras claves de un texto y clasificarlo en clases mucho más específicas, como podría ser por carrera o por temática. Con la finalidad de mejorar el acceso a la información y acortar los tiempos que le toma a un estudiante promedio encontrar trabajos académicos que le puedan ayudar en su vida universitaria.
+
+<!-- TEAM MEMBERS -->
+## Integrantes
+
+- Samuel Cano (U202116508@upc.edu.pe) 🐱
+- Eduardo Puglisevich (U20201e850@upc.edu.pe)
+- Nicolás Guerrero (U202115535@upc.edu.pe)
---
+++ source_code/app.py
@@ -0,0 +1,129 @@
+from flask import Flask, request, render_template, jsonify
+import pandas as pd
+import joblib
+from keras.models import load_model
+import os
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+import numpy as np
+from sklearn.decomposition import PCA
+
+app = Flask(__name__)
+
+# Cargar modelos y escaladores al iniciar la aplicación
+modelos = {}
+scalers = {}
+crypto_list = pd.read_csv('data/clasificacion.csv')['symbol'].tolist()
+
+# Cargar modelos LSTM y escaladores
+for symbol in crypto_list:
+    modelos[symbol] = load_model(f'data/modelos_guardados/modelo_{symbol}.keras')
+    scalers[symbol] = {}
+    for col in ['volume_24h', 'close', 'open', 'high', 'low', 'market_cap']:
+        scalers[symbol][col] = joblib.load(f'data/escaladores_guardados/scaler_{symbol}_{col}.pkl')
+
+# Cargar modelo de clasificación
+clf = joblib.load('data/RandomForest.pkl')
+
+# Cargar datos históricos de criptomonedas
+crypto_data = pd.read_csv('data/cmcHistorical(2024)(Model).csv')
+
+
+# Load cryptocurrencies from a CSV file
+cryptocurrencies = pd.read_csv('data/criptomonedas.csv')['symbol'].tolist()
+
+@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
+
+def predict():
+    symbol = request.form['symbol']
+    symbol_data = crypto_data[crypto_data['symbol'] == symbol]
+    
+    avg_volatility_daily = symbol_data['log_return'].std()
+    total_volatility = avg_volatility_daily * np.sqrt(len(symbol_data))
+    price_variation_multiplier = (symbol_data["close"].max() - symbol_data["close"].iloc[0]) / symbol_data["close"].iloc[0]
+    avg_daily_market_cap = symbol_data["market_cap"].mean()
+    max_drawdown = symbol_data["drawdown"].min()
+    avg_volume_24h = symbol_data["volume_24h"].mean()
+    
+    dictCrypto = {
+        "symbol": symbol,
+        "total_volatility": total_volatility,
+        "price_variation_multiplier": price_variation_multiplier,
+        "avg_daily_market_cap": avg_daily_market_cap,
+        "max_drawdown": max_drawdown,
+        "avg_volume_24h": avg_volume_24h
+    }
+    dfClasTest = pd.DataFrame([dictCrypto])
+
+    X_test = dfClasTest.drop(columns=['symbol', 'price_variation_multiplier'])
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    X_test_scaled = scaler.fit_transform(X_test)
+    clf_prediction = clf.predict(X_test_scaled)
+
+    clases = pd.read_csv('data/clasificacion.csv')
+    symbol_list = clases[clases["type"] == clf_prediction[0]]["symbol"].tolist()
+    #if clf_prediction[0] == "Estable":
+        #symbol_list = ["ADA","AMB"]
+    #elif clf_prediction[0] == "Especulativa":
+        #symbol_list = ["BLZ","BNT"]
+    #elif clf_prediction[0] == "Oportunidad":
+        #symbol_list = ["GARD","STX"]   
+    predictionIncrements = []
+
+    for s in symbol_list:
+        lstm_features = symbol_data[['open', 'high', 'low', 'close', 'volume_24h', 'market_cap']].copy()
+        
+        # Normalizar las características usando los escaladores específicos
+        for col in lstm_features.columns:
+            lstm_features[col] = scalers[s][col].transform(lstm_features[[col]])
+
+        pca = PCA(n_components=3)
+        pca_data = lstm_features.values
+        pca_result = pca.fit_transform(pca_data)
+
+        pca_columns = [f'pca_{i+1}' for i in range(pca.n_components_)]
+        df_pca = pd.DataFrame(pca_result, columns=pca_columns, index=lstm_features.index)
+        lstm_features = df_pca.copy()
+        volatility_scaler = MinMaxScaler(feature_range=(0, 1))
+        volatility_df = pd.DataFrame(symbol_data['volatility'])  # Asegúrate de pasar un DataFrame
+        lstm_features['volatility'] = volatility_scaler.fit_transform(volatility_df)
+        lstm_features.reset_index(drop=True, inplace=True)
+        
+        features = ['pca_1', 'pca_2', 'pca_3', 'volatility']    
+        
+        recent_data = lstm_features[features].values[-120:]  # Obtener los últimos 90 días de datos
+        recent_data = recent_data.reshape((1, 120, len(features)))
+
+        # Predicción a 90 días en el futuro
+        predictions = []
+        for _ in range(210):
+            next_prediction = modelos[s].predict(recent_data)
+            predictions.append(next_prediction[0][0])
+
+            # Crear un array con los valores necesarios para actualizar recent_data
+            next_prediction_scaled = np.zeros(len(features))
+            next_prediction_scaled[0] = next_prediction[0][0]  # Asumiendo que la predicción es del primer PCA
+
+            # Actualizar recent_data para incluir la nueva predicción y eliminar el día más antiguo
+            recent_data = np.append(recent_data[:, 1:, :], next_prediction_scaled.reshape((1, 1, len(features))), axis=1)
+
+        # Desnormalizar las predicciones
+        predicted_prices = scalers[s]['close'].inverse_transform(np.array(predictions).reshape(-1, 1))
+        predictionIncrements.append((predicted_prices.max() - symbol_data['close'].iloc[0]) / symbol_data['close'].iloc[0] * 100)
+
+    worst_result = min(predictionIncrements)
+    best_result = max(predictionIncrements)
+    return jsonify({
+        'symbol': symbol,
+        'worst_result': worst_result,
+        'best_result': best_result
+    })
+
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+@app.route('/recommender')
+def recommender():
+    return render_template('recommender.html', cryptos=cryptocurrencies)    
